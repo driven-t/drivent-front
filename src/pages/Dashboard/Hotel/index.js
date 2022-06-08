1f4dcb1dae60d9ebbd26d3fd3fd7590debd74aef
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-06-08 16:33:02 -0300

    feat: get hotels info

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,10 @@
+import { useEffect } from 'react';
+import useHotel from '../../../hooks/api/useHotel';
+
 export default function Hotel() {
+  const { hotels } = useHotel();
+
+  console.log(hotels);
+
   return 'Hotel: Em breve!';
 }
